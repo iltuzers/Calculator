82edd24dfce425a7faac1808dc345b0a27d3a124
servet iltuzer <iltuzers@gmail.com>
2022-11-15 08:03:43 -0500

    Added infix_to_postfix method

--- calculator.py
+++ calculator.py
@@ -1,7 +1,50 @@
 from Stack import Stack
+import sys
 
+
+"""
+Algorithm:
+1. If (
+2. If ), then pop the stack until (
+3. If 0..9, then add it to the list
+
+
+"""
 def infix_to_postfix(infix_exp):
-    ...
+    infix = parse_exp(infix_exp)
+    postfix_list = []
+    operator_stack = Stack()
+    prec = {"/": 2, "*": 2, "+": 1, "-": 1, "(": 0} # Need it to compare precedence of Stack symbols
+
+    for i in infix:
+        if i in "(/*+-":
+            if not operator_stack.is_empty():
+                top = operator_stack.peek()
+                while prec[i] <= prec[top]:
+                    top = operator_stack.pop()
+                    postfix_list.append(top)
+            operator_stack.push(i)
+
+        elif i == ")":
+            try:
+                top = operator_stack.pop()
+                while top != "(":
+                    postfix_list.append(top)
+                    top = operator_stack.pop()
+            except IndexError:
+                sys.exit("Not a valid expression")
+            
+        else:
+            postfix_list.append(i)
+    
+    while not operator_stack.is_empty():
+        postfix_list.append(operator_stack.pop())
+    
+    return "".join(postfix_list)
+
+
+
+
 
 def evaluate_postfix(postfix_exp):
     ...
